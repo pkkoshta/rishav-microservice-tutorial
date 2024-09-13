fbb424afe7a9c6db97b892cc297f97eedec229a2
pkkoshta <prashant30koshta@gmail.com>
2024-09-13 15:25:57 +0530

    eureka-server

---
+++ src/test/java/com/prashant/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashant.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
